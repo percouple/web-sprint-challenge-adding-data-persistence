65191d9411646b018c596ddb33ce163e03db5b07
Percouple <calebsevan@gmail.com>
2024-03-14 12:06:30 -0500

    Initial router wiring

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,19 @@
-// build your `/api/projects` router here
+// // build your `/api/projects` router here
+const express = require('express');
+const router = express.Router();
+
+// - [ ] `[POST] /api/projects`
+//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+//   - Example of response body: `{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}`
+router.post('/', (req, res) => {
+    res.status(200).json({"project_id":1,"project_name":"bar","project_description":null,"project_completed":false})
+})
+
+// - [ ] `[GET] /api/projects`
+//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+//   - Example of response body: `[{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]`
+router.get('/', (req, res) => {
+    res.status(200).json({"project_id":1,"project_name":"bar","project_description":null,"project_completed":false})
+})
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,17 @@
-// build your `/api/resources` router here
+// // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+
+// - [ ] `[GET] /api/resources`
+//   - Example of response body: `[{"resource_id":1,"resource_name":"foo","resource_description":null}]`
+router.get('/', (req, res) => {
+    res.status(200).json({ "resource_id": 1, "resource_name": "foo", "resource_description": null })
+})
+
+// - [ ] `[POST] /api/resources`
+//   - Example of response body: `{"resource_id":1,"resource_name":"foo","resource_description":null}`
+router.post('/', (req, res) => {
+    res.status(200).json({ "resource_id": 1, "resource_name": "foo", "resource_description": null })
+})
+
+module.exports = router;
